5cec397311627354ce576ce05a0d4f91502b47c9
Abdullah <abdullahxoff@gmail.com>
2025-10-22 19:49:37 +0300

    fix: correct expiration date calculation in generateToken method

--- src/main/java/com/crud/demo/security/JwtUtil.java
+++ src/main/java/com/crud/demo/security/JwtUtil.java
@@ -17,7 +17,7 @@ public class JwtUtil {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1d
+                .setExpiration(new Date()) // System.currentTimeMillis() + 86400000
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
